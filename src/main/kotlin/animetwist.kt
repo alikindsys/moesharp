84ff6ddbeae3e20ca0c785061c2d1392418d0fed
Paulo Henrique Prates <t3ctotalmenterandom1@outlook.com>
2020-10-01 23:06:20 -0300

    Make animetwist.kt extension functions internal

--- src/main/kotlin/animetwist.kt
+++ src/main/kotlin/animetwist.kt
@@ -20,7 +20,7 @@ data class MoeAnime(val link: Uri, val episodeCount: Int, val name: String) {
 
 data class MoeAnimeEpisode(val anime: MoeAnime, val episodeNumber: Int, val url: String)
 
-fun WebDriver.getEpisodes(anime: MoeAnime) : List<MoeAnimeEpisode> {
+internal fun WebDriver.getEpisodes(anime: MoeAnime) : List<MoeAnimeEpisode> {
     val episodes: MutableList<MoeAnimeEpisode> = mutableListOf()
     println("Getting episodes for : [${anime.name}]")
     for( i in 1..anime.episodeCount){
@@ -48,19 +48,19 @@ fun WebDriver.getEpisodes(anime: MoeAnime) : List<MoeAnimeEpisode> {
     return episodes.toList()
 }
 
-fun WebDriver.getAnimeSize(link: String) : Int {
+internal fun WebDriver.getAnimeSize(link: String) : Int {
     this.get(link)
     val e = this.findElement(By.ByXPath("/html/body/div[1]/div[2]/div/div[1]/section/main/div[2]/div[3]"))
     val episodes = e.findElements(By.tagName("li"))
     return episodes.count()
 }
 
-fun WebDriver.getAnimeName(link: String) : String {
+internal fun WebDriver.getAnimeName(link: String) : String {
     this.get(link)
     val e = this.findElement(By.xpath("/html/body/div[1]/div[2]/div/div[1]/section/main/div[2]/div[1]/h2/span"))
     return e.text.trim()
 }
-fun String.processLink() : String {
+internal fun String.processLink() : String {
     val processedLink : MutableList<String> = mutableListOf()
     for (s in this.split("/")) {
         if(s.contains(Regex("\\d+"))) continue
@@ -69,11 +69,11 @@ fun String.processLink() : String {
     return processedLink.joinToString(separator = "/")
 }
 
-fun String.isAnimeTwist() : Boolean {
+internal fun String.isAnimeTwist() : Boolean {
     return this.contains("https://twist.moe/a/")
 }
 
-fun WebDriver.getAllAnimes(args:Array<String>) : List<MoeAnime> {
+internal fun WebDriver.getAllAnimes(args:Array<String>) : List<MoeAnime> {
     val tempAnimes = mutableListOf<MoeAnime>()
     for(s in args){
         if(!s.isAnimeTwist()){
@@ -87,7 +87,7 @@ fun WebDriver.getAllAnimes(args:Array<String>) : List<MoeAnime> {
     return tempAnimes.toList()
 }
 
-fun MoeAnimeEpisode.download(config: Config) {
+internal fun MoeAnimeEpisode.download(config: Config) {
     val file = File("${config.animepath}/${this.anime.name}/${this.episodeNumber}.mp4")
     val path = File("${config.animepath}/${this.anime.name}")
     println("[${this.episodeNumber}/${this.anime.episodeCount}] Downloading ${this.anime.name} - Episode ${this.episodeNumber} to [${file.canonicalPath}]")
@@ -116,16 +116,16 @@ fun MoeAnimeEpisode.download(config: Config) {
     }
 }
 
-fun decodeURL(str: String) : String {
+internal fun decodeURL(str: String) : String {
     return str.replace("%2520", " ").replace("%5B", "[").replace("%5D", "]")
 }
 
-fun getKnownCDNServer() : String {
+internal fun getKnownCDNServer() : String {
     val rnd = Random.nextInt(1, 60)
     return "https://edge-$rnd.cdn.bunny.sh"
 }
 
-fun InputStream.writeAllToFile(file : File) {
+internal fun InputStream.writeAllToFile(file : File) {
     val fos = FileOutputStream(file, true)
     val buffer = ByteArray(100 * 1024)
     var bytesRead: Int
@@ -135,7 +135,7 @@ fun InputStream.writeAllToFile(file : File) {
     fos.closeQuietly()
 }
 
-fun MoeAnimeEpisode.getLength(url : URL) : Long {
+internal fun MoeAnimeEpisode.getLength(url : URL) : Long {
     val conn = url.openConnection() as HttpURLConnection
     conn.requestMethod = "HEAD"
     conn.setRequestProperty("referer", "${this.anime.link}/${this.episodeNumber}")
@@ -144,7 +144,7 @@ fun MoeAnimeEpisode.getLength(url : URL) : Long {
     return size
 }
 
-fun MoeAnimeEpisode.getInputStreamRequest(url : URL, file: File, length : Long) : HttpURLConnection? {
+internal fun MoeAnimeEpisode.getInputStreamRequest(url : URL, file: File, length : Long) : HttpURLConnection? {
     val conn = url.openConnection() as HttpURLConnection
     conn.requestMethod = "GET"
     conn.setRequestProperty("referer", "${this.anime.link}/${this.episodeNumber}")
